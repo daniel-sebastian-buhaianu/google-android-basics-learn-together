57a91bd6667d51cce039fb9f5226ed7ee261e074
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-04-10 15:05:02 +0300

    added Image and modifiers

--- app/src/main/java/com/example/learntogether/MainActivity.kt
+++ app/src/main/java/com/example/learntogether/MainActivity.kt
@@ -3,14 +3,21 @@ package com.example.learntogether
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.learntogether.ui.theme.LearnTogetherTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,7 +30,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    // main function call
+                    AppLayout(stringResource(R.string.app_title))
                 }
             }
         }
@@ -31,14 +38,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Title(titleName: String) {
-    Text(text = "$titleName")
+fun AppLayout(title: String) {
+    val image = painterResource(id = R.drawable.bg_compose_background)
+
+    Column {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Text(
+            text = "$title",
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(16.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun TitlePreview() {
+fun AppLayoutPreview() {
     LearnTogetherTheme {
-        Title(stringResource(R.string.app_title))
+        AppLayout(stringResource(R.string.app_title))
     }
 }
